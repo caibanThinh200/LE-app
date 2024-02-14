cca389dff8e720d252029d24fcda0ff983cdef1f
NguyenQuocThinh13 <thinh@saigon.digital>
2024-02-14 12:28:02 +0700

    add cors origin

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -6,7 +6,11 @@ async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
   // app.useStaticAssets(join(__dirname, '..', 'upload'));
   app.setGlobalPrefix('/le/api/v1');
-  app.enableCors();
+  app.enableCors({
+    allowedHeaders: ['content-type'],
+    origin: [process.env.CLIENT_URL],
+    credentials: true,
+  });
   await app.listen(8888);
 }
 bootstrap();
