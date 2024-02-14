b5aa61153d9a6b3d418873cce590d9fe176eb217
NguyenQuocThinh13 <thinh@saigon.digital>
2024-02-14 18:40:25 +0700

    add accept header

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -5,12 +5,11 @@ import { NestExpressApplication } from '@nestjs/platform-express';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
-  // app.useStaticAssets(join(__dirname, '..', 'upload'));
   app.setGlobalPrefix('/le/api/v1');
   app.enableCors({
     origin: [process.env.CLIENT_URL || 'https://le-app-navy.vercel.app'],
     credentials: true,
-    allowedHeaders: ['Content-Type', 'Authorization'],
+    // allowedHeaders: ['Content-Type', 'Authorization', 'Accept'],
     // methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     // preflightContinue: false,
     // optionsSuccessStatus: 204,
